a5eb7d7a393e45e5d2fee0767d958aacd695e791
sassok <kevin.sassoo@gmail.com>
2021-06-16 20:08:52 +0200

    change owners migration for fix bug

--- db/migrate/20210616091446_add_column_to_owners.rb
+++ db/migrate/20210616091446_add_column_to_owners.rb
@@ -1,8 +1,8 @@
 class AddColumnToOwners < ActiveRecord::Migration[6.1]
    def change
     create_table :add_column_to_owners do |t|
-      add_reference :owners, :agency, foreign_key: true
-      add_reference :owners, :building, foreign_key: true
+      add_reference :owners, :agency
+      add_reference :owners, :building
       add_column :owners, :first_name, :string
       add_column :owners, :last_name, :string
       add_column :owners, :phone_number, :string
