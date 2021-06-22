980b24b9f2acab95d5767a1ad7a475b6f87027aa
sassok <kevin.sassoo@gmail.com>
2021-06-22 17:11:40 +0200

    fix permit for create Event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -46,6 +46,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:title, :description, :datetime, :duration)
+      params.require(:event).permit(:agency_id, :building_id, :title, :description, :datetime, :duration)
     end
 end
